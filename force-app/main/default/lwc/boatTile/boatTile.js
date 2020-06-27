5da35c613430d7a0135c55d77cd581b51441d2c8
matthew dennin <mdennin1@gmail.com>
2020-06-27 12:59:23 -0400

    step 6 complete

--- force-app/main/default/lwc/boatTile/boatTile.js
+++ force-app/main/default/lwc/boatTile/boatTile.js
@@ -1,11 +1,16 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
+import { publish, MessageContext, subscribe } from 'lightning/messageService';
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
 
 const TILE_WRAPPER_SELECTED_CLASS = 'tile-wrapper selected';
-const TILE_WRAPPER_UNSELECTED_CLASS = 'tile-wrapper'
+const TILE_WRAPPER_UNSELECTED_CLASS = 'tile-wrapper';
 
 export default class BoatTile extends LightningElement {
     @api boat;
     @api selectedBoatId;
+
+    @wire(MessageContext)
+    messageContext;
     
     // Getter for dynamically setting the background image for the picture
     get backgroundStyle() { 
@@ -22,6 +27,8 @@ export default class BoatTile extends LightningElement {
     selectBoat(event) { 
         console.log(`selectBoat() fired`);
         this.dispatchEvent(new CustomEvent('boatselect', { detail: { boatId: this.boat.Id }}));
+        const message = { recordId: this.boat.Id };
+        publish(this.messageContext, BOATMC, message);
     }
 
   }
