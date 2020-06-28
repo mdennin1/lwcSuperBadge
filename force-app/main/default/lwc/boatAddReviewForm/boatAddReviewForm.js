ffc34442de3f05f394883d775c64b0ee7e580e52
matthew dennin <mdennin1@gmail.com>
2020-06-28 18:00:47 -0400

    og boatAddRevieForm

--- force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
+++ force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class BoatAddReviewForm extends LightningElement {}
+export default class BoatAddReviewForm extends LightningElement {
+    // Private
+    boatId;
+    rating;
+    
+    // Public Getter and Setter to allow for logic to run on recordId change
+    @api
+    get recordId() { }
+    set recordId(value) {
+      //sets boatId attribute
+      //sets boatId assignment
+    }
+    
+    // Gets user rating input from stars component
+    handleRatingChanged(event) { }
+    
+    // Custom submission handler to properly set Rating
+    // This function must prevent the anchor element from navigating to a URL.
+    // form to be submitted: lightning-record-edit-form
+    handleSubmit(event) { }
+    
+    // Shows a toast message once form is submitted successfully
+    // Dispatches event when a review is created
+    handleSuccess() {
+      // TODO: dispatch the custom event and show the success message
+      this.handleReset();
+    }
+    
+    // Clears form data upon submission
+    // TODO: it must reset each lightning-input-field
+    handleReset() { }
+  }
